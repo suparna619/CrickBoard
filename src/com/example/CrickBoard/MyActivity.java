e22131acb8ac5f4fcf6a8ddee715e77c304ada1e
suparna619 <suparnad@thoughtworks.com>
2015-08-26 21:20:23 +0530

    submit button created for to show text

--- src/com/example/CrickBoard/MyActivity.java
+++ src/com/example/CrickBoard/MyActivity.java
@@ -2,9 +2,11 @@ package com.example.CrickBoard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -28,6 +30,20 @@ public class MyActivity extends Activity {
                 return handled;
             }
         });
+        final String[] text = {""};
+        Button button = (Button) findViewById(R.id.Submit);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                TextView foo = (TextView)findViewById(R.id.formattedtext);
+                String text = editText.getText().toString();
+                foo.setText(text);
+//                 text[0] = String.valueOf(editText.getText());
+            }
+
+
+        });
+        System.out.print(text[0]);
     }
 
 }
