a53d576dcbb90e255d1ba2bae9de3cc3685048a6
suparna619 <suparnad@thoughtworks.com>
2015-08-26 20:30:14 +0530

    text box is showing and the app is running fine

--- src/com/example/CrickBoard/MyActivity.java
+++ src/com/example/CrickBoard/MyActivity.java
@@ -2,6 +2,8 @@ package com.example.CrickBoard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +13,22 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pirates:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.radio_ninjas:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
